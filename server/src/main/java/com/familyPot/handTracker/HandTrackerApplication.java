aef66df418fc55ebb6e728d3c1b21801591480c4
George Sheng <gsheng112@gmail.com>
2024-05-14 03:52:34 -0400

    Initial commit

---
+++ server/src/main/java/com/familyPot/handTracker/HandTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.familyPot.handTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/familyPot/handTracker/HandTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyPot.handTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
